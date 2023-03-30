4ec77b9254830b3050f77beda0e8cc643940f073
Priyadarshini R <Priyadarshini.R@gds.ey.com>
2023-03-30 13:53:34 +0530

    second commit

--- main.tf
+++ main.tf
@@ -16,8 +16,7 @@ resource "aws_rds_cluster" "default" {
   engine                  = "aurora-mysql"
   engine_version          = "5.7.mysql_aurora.2.03.2"
   availability_zones      = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  database_name           = "mydb
-  
+  database_name           = "mydb"
   master_username         = "foo"
   master_password         = "bar"
   backup_retention_period = 5
