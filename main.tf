5dad447c72772d2c562aabe832077ac25c456a0d
Priyadarshini R <Priyadarshini.R@gds.ey.com>
2023-04-06 16:51:26 +0530

    first commit

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "4.60.0"
+      version = "4.61.0"
     }
   }
 }
@@ -11,14 +11,8 @@ provider "aws" {
   # Configuration options
 }
 
-resource "aws_rds_cluster" "default" {
-  cluster_identifier      = "aurora-cluster-demo"
-  engine                  = "aurora-mysql"
-  engine_version          = "5.7.mysql_aurora.2.03.2"
-  availability_zones      = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  database_name           = "mydb"
-  master_username         = "foo"
-  master_password         = "bar"
-  backup_retention_period = 5
-  preferred_backup_window = "07:00-09:00"
+resource "aws_sns_topic" "user_updates" {
+  name              = "user-updates-topic"
+  kms_master_key_id = "enabled"
 }
+
