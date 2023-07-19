bf1724a3683ae15c528cb4815e8440f7e6ac0cb0
Priyadarshini66 <priyaraj66.r@gmail.com>
2023-07-19 15:14:01 +0530

    firstcommit

--- main.tf
+++ main.tf
@@ -1,18 +1,12 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "4.61.0"
-    }
-  }
-}
-
-provider "aws" {
-  # Configuration options
-}
-
-resource "aws_sqs_queue" "terraform_queue" {
-  name                              = "terraform-example-queue"
-  kms_master_key_id                 = "enabled"
- 
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  db_name              = "mydb"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+  publicly_accessible = true
 }
